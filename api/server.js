658a63fb24da6252a4b4c656c96bb6f0d0844f16
stevenjhomem <sjhomem@outlook.com>
2021-10-05 10:17:13 -0500

    setting up prettier

--- api/server.js
+++ api/server.js
@@ -1,7 +1,7 @@
-const express = require("express")
+const express = require("express");
 
-const server = express()
+const server = express();
 
 // DO YOUR MAGIC
 
-module.exports = server
+module.exports = server;
