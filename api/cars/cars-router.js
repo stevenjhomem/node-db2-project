33b553b24f2cd36eb6b5e1621da4ec30e6104f4f
stevenjhomem <sjhomem@outlook.com>
2021-10-05 14:51:31 -0500

    got my carsRouter working and being read

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,1 +1,17 @@
-// DO YOUR MAGIC
+const express = require("express");
+
+const carsRouter = express.Router();
+
+carsRouter.get("/", async (req, res, next) => {
+  res.json("getting all cars");
+});
+
+carsRouter.get("/:id", async (req, res, next) => {
+  res.json(`getting all cars with id ${req.params.id}`);
+});
+
+carsRouter.post("/", async (req, res, next) => {
+  res.json("posting new car");
+});
+
+module.exports = carsRouter;
